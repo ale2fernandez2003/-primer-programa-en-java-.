e5057f7280e04b4f987de0b95f6746ed9e3e3fe9
ale2fernandez2003 <92783863+ale2fernandez2003@users.noreply.github.com>
2021-10-19 12:16:55 +0200

    primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+class HolaMundo {
+  public static void main(String [] args){
+	System.out.println("Hola mundo");
+	System.out.println("Saludos desde León");
+	System.out.println("Nacimiento en la localidad de León");
+	System.out.println("Año de nacimiento en 2003");
+  }
+}
